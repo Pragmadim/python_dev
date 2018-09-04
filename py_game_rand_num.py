200d87b88004b69604c9560263d7250d394dc933
Pragmadim <pul.95@mail.ru>
2018-09-04 13:24:36 +0300

    ver. 0.2

--- py_game_rand_num.py
+++ py_game_rand_num.py
@@ -1,38 +1,37 @@
 import random
 
-print('Welcome to my first Python game! Guess random generated number from 1 to 10!')
-
-gen = input('Print generate to begin playing! \n')
-
 
 def begin():
-    l = 3  ## Кол-во жизней
     r_num = random.randint(1, 10)
     p_num = int(input('Enter your number: '))
-    while l > 0:
-        if p_num == r_num:
-            print("You've won! Excellent!")
-            play_again = input("Do you want to play again? y/n")
-            if play_again == 'y' or play_again == 'Y':
-                begin()
-            else:
-                print('Goodbye!')
-        elif p_num < r_num:
-            print("Your number is lower than it must be!")
-            l -= 1
-            print('You lost your life! Now you have only {}'.format(l))
+    ch = (r_num, p_num)
+    if ch[0] == ch[1]:
+        print(ch[0])
+        print("You've won! Excellent!")
+        play_again = input("Do you want to play again? y/n")
+        if play_again == 'y' or play_again == 'Y':
             begin()
-        elif p_num > r_num:
-            print("Your number is higher than it must be!")
-            l -= 1
-            print('You lost your life! Now you have only {}'.format(l))
-            begin()
-        elif l == 0:
-            print("Game Over! Your HP ended.")
+        elif play_again == 'n' or play_again == 'N':
+            print('Goodbye!')
+    elif ch[1] < ch[0]:
+        print(ch[0])
+        print("Your number is lower than it must be!")
+        begin()
+    elif ch[1] > ch[0]:
+        print(ch[0])
+        print("Your number is higher than it must be!")
+        begin()
 
-if gen == 'generate':
-    print('Success!')
-    begin()
-else:
-    print('Fail!')
-    print("Type 'Generate' to begin playing!")
+
+def start():
+    gen = input('Print generate to begin playing! \n')
+    if gen == 'generate':
+        print('Success!')
+        begin()
+    else:
+        print('Fail!')
+        start()
+
+
+print('Welcome to my first Python game! Guess random generated number from 1 to 10!')
+start()
